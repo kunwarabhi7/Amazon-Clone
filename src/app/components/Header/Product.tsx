008bdcf2d6b2d1732ca0107a31426b1205bd38dc
Abhishek <abhishekparmar162@gmail.com>
2023-10-21 17:08:21 +0530

    added stying to product page

--- src/app/components/Header/Product.tsx
+++ src/app/components/Header/Product.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { ProductProps } from "../../../../type";
-
+import { BiSolidCart } from "react-icons/bi";
+import { BsChatSquareHeartFill } from "react-icons/bs";
 const Product = ({ product }: any) => {
   const {
     id,
@@ -13,39 +13,40 @@ const Product = ({ product }: any) => {
     image,
     brand,
   } = product;
-  console.log(product);
   return (
-    <div className="bg-white rounded-lg shadow-lg">
-      <div className="relative overflow-hidden">
-        {isNew && (
-          <div className="absolute top-0 left-0 bg-primary text-white py-1 px-3 text-xs font-semibold rounded-tl-md">
-            New
-          </div>
-        )}
+    <div
+      key={id}
+      className="bg-white rounded-lg shadow-lg w-full p-4 border border-x-gray-300 group  overflow-hidden"
+    >
+      <div className="relative w-full h-[260px] overflow-hidden">
         <Image
           src={image}
           alt={title}
-          width={150}
-          height={150}
-          className="w-full h-[400px] object-cover"
+          width={300}
+          height={300}
+          className="w-full h-full object-cover scale-90 hover:scale-100  duration-300"
         />
+        <div className="absolute top-20 -right-20 flex flex-col  group-hover:left-[310px] duration-300">
+          <span className="text-cyan-300 cursor-pointer">
+            <BiSolidCart size={30} />
+          </span>
+          <span className="h-12 w-12 text-red-500 cursor-pointer">
+            <BsChatSquareHeartFill size={30} />
+          </span>
+        </div>
       </div>
-      <div className="p-4">
-        <div className="text-gray-700 text-sm">{category}</div>
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p className="text-gray-600 text-sm">{description}</p>
-        <div className="mt-2">
-          {oldPrice && (
-            <p className="text-gray-400 text-sm line-through">${oldPrice}</p>
-          )}
-          <p className="text-xl text-primary font-semibold">${price}</p>
+      <hr />
+      <div className="p-2 px-4">
+        <p className="text-gray-600">{category}</p>
+        <p className="my-2 text-xl font-bold">{title}</p>
+        <div className="flex items-center space-x-8">
+          <p className="line-through">${oldPrice}</p>
+          <p className="font-bold">${price}</p>
         </div>
-        <div className="mt-4 flex justify-between items-center">
-          <span className="text-gray-500 text-sm">{brand}</span>
-          <button className="text-primary text-sm font-semibold hover:underline">
-            View Details
-          </button>
-        </div>
+        <p className="text-sm">{description}</p>
+        <button className="bg-black text-white w-full rounded-full mx-2 mt-4 p-1">
+          Add to Cart
+        </button>
       </div>
     </div>
   );
