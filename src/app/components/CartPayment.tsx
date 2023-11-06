936f088e7fa6550b6f763811740a6b4b7d90a430
Abhishek <abhishekparmar162@gmail.com>
2023-11-06 08:59:37 +0530

    fix cart total

--- src/app/components/CartPayment.tsx
+++ src/app/components/CartPayment.tsx
@@ -1,10 +1,10 @@
 const CartPayment = () => {
   return (
-    <div className="mx-32 mt-12 bg-white rounded-md h-40 w-64  pt-4">
-      <p className="ml-4 text-xl font-semibold">
+    <div className="xl:mx-8 mx-6 mt-12 bg-white pl-8 rounded-md h-40 xl:h-64 xl:w-96 w-64  pt-4">
+      <p className="ml-4 text-xl font-semibold pt-8 ">
         Subtotal <span>1 item : 3,000Rs</span>
       </p>
-      <button className="ml-4 mt-5 w-48 bg-yellow-400 rounded-lg p-2 hover:bg-yellow-500 ">
+      <button className="ml-4 mt-5 xl:w-48 bg-yellow-400 rounded-lg xl:p-2 p-1 hover:bg-yellow-500 ">
         Proceed to Buy
       </button>
     </div>
