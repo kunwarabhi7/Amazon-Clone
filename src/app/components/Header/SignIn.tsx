969941241ff502720c60973abd658c3716e703be
Abhishek <abhishekparmar162@gmail.com>
2023-11-17 14:49:39 +0530

    new changes

--- src/app/components/Header/SignIn.tsx
+++ src/app/components/Header/SignIn.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import { BiSolidDownArrow } from "react-icons/bi";
 const SignIn = () => {
   const { data: session } = useSession();
   console.log(session);
   if (session)
     return (
-      <div className="cursor-pointer" onClick={() => signOut()}>
-        {session.user?.name}
+      <div
+        className="cursor-pointer flex items-center px-2 border border-transparent hover:border-white duration-300 gap-1 h-[70%]"
+        onClick={() => signOut()}
+      >
+        <Image
+          height={10}
+          width={10}
+          className="w-10 h-10 rounded-full object-cover"
+          src={session.user?.image!}
+          alt={session.user?.name!}
+        />
+        <div>
+          <h1>{session.user?.name}</h1>
+          <h1>{session.user?.email}</h1>
+        </div>
       </div>
     );
   return (
